147f68203ba9ff996a108f2d90e9438bfefd5bbd
luciadesperati <126161614+luciadesperati@users.noreply.github.com>
2023-05-13 12:00:16 +0100

    Showing just the first definition for each meaning

--- src/Definitions.js
+++ src/Definitions.js
@@ -9,15 +9,17 @@ export default function Definitions(props) {
       <p className="mt-5">
         <strong>{props.meaning.partOfSpeech}</strong>
       </p>
-      {props.meaning.definitions.map(function (definition, index) {
+      
+      <SingleDefinition definition={props.meaning.definitions[0]} />
+
+      {/* {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <SingleDefinition definition={definition} />
             {index !== props.meaning.definitions.length - 1 && <hr />}{" "}
-            {/* Add hr except for the last index */}
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 }
